138d6b2a36990ca3bdcf80e72dd3c75a4f00b411
shutrup <magagaraev95@mail.ru>
2023-11-13 08:44:33 +0300

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Шарап Бамматов on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
